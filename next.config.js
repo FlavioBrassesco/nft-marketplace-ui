5f60c93b50d2770114bbacf4754781acadd332f4
Flavio Brassesco <brassescoflavio@gmail.com>
2022-08-19 11:29:51 -0300

    fixed config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,6 @@ const path = require("path");
 
 module.exports = {
   reactStrictMode: true,
-  env: {
-    mongodburl:
-      "mongodb+srv://flaviobrasso:M0n60db%21789@notes.tskkj.mongodb.net/marketplace?retryWrites=true&w=majority",
-  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
